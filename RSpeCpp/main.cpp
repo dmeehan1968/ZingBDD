fdc45ef3e8cf7537abe31db03b3e5c88675dc71d
Dave Meehan <dave_meehan@replicated.co.uk>
2014-01-05 20:27:52 +0100

    Initial Commit

---
+++ RSpeCpp/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  RSpeCpp
+//
+//  Created by Dave Meehan on 05/01/2014.
+//  Copyright (c) 2014 Replicated Solutions Limited. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[])
+{
+
+	// insert code here...
+	std::cout << "Hello, World!\n";
+    return 0;
+}
+
